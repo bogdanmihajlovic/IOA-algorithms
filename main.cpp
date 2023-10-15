b4678b5716aefb22d6d84b7577de41b5b761fefb
Bogdan Mihajlovic <bogdanmihajlovic4@gmail.com>
2023-10-15 18:13:22 +0200

    Added nTuples and kCombinations

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "permutation.hpp"
+
+int main(){
+    //nTuples(3, 4);
+    kCombinations(5, 2);     
+}
---
+++ permutation.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+/*Variations with repetition n^k*/
+void nTuples(int n, int k);
+
+/*Combinations without repetition*/
+void kCombinations(int n, int k);
+
